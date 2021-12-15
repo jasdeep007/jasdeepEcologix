3cc7360857d46bdf310823a8ee006cf760bd82ac
jasdeep singh <jasdeep.chauhan@hotmail.com>
2021-12-15 14:27:14 -0600

    Point 7 done in this commit

---
+++ jasdeepConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace jasdeepConsole
+{
+    public static class QuestionClass
+    {
+        static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John",
+        };
+        public static void loopwithoutforeachandfor()
+        {
+            using (var loop = NamesList.GetEnumerator())
+            {
+                while(loop.MoveNext())
+                {
+                    Console.WriteLine(loop.Current);
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Below string values are printed without foreach and for loop");
+            QuestionClass.loopwithoutforeachandfor();
+        }
+    }
+}
