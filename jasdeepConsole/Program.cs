b5a8274e14824c94635a31cd4734dad70e4facf6
jasdeep singh <jasdeep.chauhan@hotmail.com>
2021-12-15 15:33:43 -0600

    Point 8 done

--- jasdeepConsole/Program.cs
+++ jasdeepConsole/Program.cs
@@ -15,11 +15,30 @@ namespace jasdeepConsole
         {
             using (var loop = NamesList.GetEnumerator())
             {
-                while(loop.MoveNext())
+                while (loop.MoveNext())
                 {
                     Console.WriteLine(loop.Current);
                 }
             }
+        }
+
+        public static object TESTModule(object value)
+        {
+            switch (value.GetType().Name)
+            {
+                case "Int32":
+                    var dataint = (Convert.ToInt32(value));
+                    return (dataint >= 1 && dataint <= 4) ? (dataint * 2) :
+                                (dataint > 4) ? (dataint * 3) : throw new Exception("Value cannot be less than 1");
+                case "Single":
+                    var datafloat = (Convert.ToSingle(value));
+                    return (datafloat == 1.0f || datafloat == 2.0f) ? 3.0f : value;
+                case "String":
+                    return value.ToString().ToUpper();
+                default:
+                    return value;
+            }
+
         }
     }
 
@@ -29,6 +48,8 @@ namespace jasdeepConsole
         {
             Console.WriteLine("Below string values are printed without foreach and for loop");
             QuestionClass.loopwithoutforeachandfor();
+            Console.WriteLine("\nBelow code will print TestModule output as per input, point 8, and its demo for input as 2");
+            Console.WriteLine(QuestionClass.TESTModule(2));
         }
     }
 }
